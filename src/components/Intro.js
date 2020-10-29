165dcdee826ddee9d04995e4cb735808e13516d7
Imani Paul <imani.paul1@gmail.com>
2020-10-28 22:04:58 -0400

    Intro component

---
+++ src/components/Intro.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Intro() {
+  return (
+    <section className="intro">
+      This is the intro. Click the button to start the quiz.
+      <button>Start</button>
+    </section>
+  );
+}
