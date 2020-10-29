721badffb8320fc6512efb4bffb4c94a7977fdc3
Imani Paul <imani.paul1@gmail.com>
2020-10-28 22:26:37 -0400

    Display answers for each question

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,14 +1,20 @@
 import React, { useEffect } from "react";
 
 export default function Question(props) {
+  let allAnswers = [props.data.correct, ...props.data.incorrect];
+
   useEffect(() => {
     console.log("current question data", props.data);
-  });
+    console.log("all answers", allAnswers);
+  }, [props.data, allAnswers]);
 
   return (
     <section className="question">
       <h1>Question # {props.currentQuestion + 1}</h1>
       <h2>{props.data.question}</h2>
+      {allAnswers.map((answer, index) => (
+        <p key={index}>{answer}</p>
+      ))}
     </section>
   );
 }
