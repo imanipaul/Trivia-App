99d727dff26317137bc44d20caf3fd92f90f8857
Imani Paul <imani.paul1@gmail.com>
2020-10-28 22:10:38 -0400

    Increase current question on input page

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,10 +1,16 @@
 import React from "react";
 
-export default function Intro() {
+export default function Intro(props) {
   return (
     <section className="intro">
       This is the intro. Click the button to start the quiz.
-      <button>Start</button>
+      <button
+        onClick={() => {
+          props.setCurrentQuestion(props.currentQuestion + 1);
+        }}
+      >
+        Start
+      </button>
     </section>
   );
 }
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,10 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import Intro from "./Intro";
 import jsonData from "../data/quizData.json";
+import Question from "./Question";
 
 export default function Quiz() {
+  const [currentQuestion, setCurrentQuestion] = useState(-1);
+
   useEffect(() => {
     console.log("json data is", jsonData);
-  });
+    console.log("current question is", currentQuestion);
+  }, [currentQuestion]);
 
-  return <section className="quiz">Quiz component</section>;
+  return (
+    <section className="quiz">
+      Quiz component
+      {currentQuestion === -1 ? (
+        <Intro
+          setCurrentQuestion={setCurrentQuestion}
+          currentQuestion={currentQuestion}
+        />
+      ) : (
+        <Question />
+      )}
+    </section>
+  );
 }
