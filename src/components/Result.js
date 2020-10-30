811d2ca99c140eec09b421e0b42bccbd2d060c05
Imani Paul <imani.paul1@gmail.com>
2020-10-29 21:08:52 -0400

    Style results page

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,16 +1,20 @@
 import React from "react";
+import "../styles/Result.scss";
 
 export default function Result(props) {
   return (
     <section className="result">
-      Results!!
-      <p>Your score is: {props.score}</p>
-      <p>Your selected answers were:</p>
-      {props.selectedAnswers.map((answer, index) => (
-        <p key={index}>
-          {index + 1} : {answer}
-        </p>
-      ))}
+      <h1>Results</h1>
+      <h2>
+        You scored <span className="score">{props.score}</span>
+      </h2>
+      <div className="result-answer-container">
+        {props.selectedAnswers.map((answer, index) => (
+          <p key={index} className="result-answer">
+            {index + 1} : {answer}
+          </p>
+        ))}
+      </div>
     </section>
   );
 }
