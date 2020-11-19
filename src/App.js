e431f2edfa4cb31847462db4a369701cd05dccca
Imani Paul <imani.paul1@gmail.com>
2020-11-18 23:59:12 -0500

    Remove unused progress tracker

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Quiz from "./components/Quiz";
 import "./App.css";
-import ProgressTracker from "./components/ProgressTracker";
 
 function App() {
   return (
     <div className="App">
-      {/* <ProgressTracker /> */}
       <Quiz />
     </div>
   );
